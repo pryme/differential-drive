1ad56e037958ae7e4c60c3b24d8848a4baaebc7c
Patrick <pryme4gh@gmail.com>
2016-03-07 00:36:30 +0000

    Changed ticks_per_meter default

--- scripts/twist_to_motors.py
+++ scripts/twist_to_motors.py
@@ -19,6 +19,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+# remove test of remote file editing
+
 import rospy
 import roslib
 from std_msgs.msg import Float32
